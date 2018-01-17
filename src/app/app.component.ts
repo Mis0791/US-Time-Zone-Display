e018c69d6da61db7cd5d124944562ee6ded1c6f5
Michelle Sturgill <isabelsturgill@comcast.net>
2018-01-17 14:22:24 -0800

    Time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  today = new Date();
+  zones: Array<string> = ['PST','MST','CST','EST'];
+  click = "";
+  onButtonClick(zone) { 
+    this.click = zone;
+    this.zones = ['PST','MST','CST','EST'];
+    console.log(zone);
+    if(!zone) {
+      this.resetTime();
+    } else {
+      this.Time();
+    }
+  }
+
+  resetTime() {
+    this.today = null;
+  }
+  
+  Time() {
+    this.today = new Date();
+  }
 }
+
